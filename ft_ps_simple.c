d1ca271e426546b4dc3e468ac843298198713e0f
Toky Mathieu Randriambololona <tokyrand@c1r12s14.42antananarivo.mg>
2026-02-21 09:35:31 +0300

    depot

--- ft_ps_simple.c
+++ ft_ps_simple.c
@@ -6,7 +6,7 @@
 /*   By: tokyrand <tokyrand@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 20:12:24 by varandri          #+#    #+#             */
-/*   Updated: 2026/02/21 09:20:37 by tokyrand         ###   ########.fr       */
+/*   Updated: 2026/02/21 09:34:37 by tokyrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,23 @@ static void	rotate_down(size_t counter,t_list **stack, t_ac_list **act_lst)
 	}
 }
 
-void	ft_ps_simple(t_list *stack_a, t_list *stack_b, t_ac_list *act_lst)
+void	ft_ps_simple(t_list **stack_a, t_list **stack_b, t_ac_list **act_lst)
 {
 	int		min_index;
 	t_list	*min;
 	size_t	size_a;
 
-	size_a = lst_size(stack_a);
+	size_a = lst_size(*stack_a);
 	while ( size_a > 2)
 	{
-		min = find_min(stack_a);
-		min_index = find_index(stack_a, min);
+		min = find_min(*stack_a);
+		min_index = find_index(*stack_a, min);
 		if (min_index <= size_a / 2 && min_index)
-			rotate_up(min_index, &stack_a, &act_lst);
+			rotate_up(min_index, stack_a, act_lst);
 		if (min_index > size_a / 2 && min_index)
-			rotate_down((size_a - min_index), &stack_a, &act_lst);
+			rotate_down((size_a - min_index), stack_a, act_lst);
 		if (!min_index)
-			push(&stack_a, &stack_b, &act_lst, "pb");
+			push(stack_a, stack_b, act_lst, "pb");
 		size_a--;
 	}
 }
