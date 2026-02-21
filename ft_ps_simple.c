32c593b106cb4ee0c68afcfcaf6df48f3451d29e
Toky Mathieu Randriambololona <tokyrand@c1r12s16.42antananarivo.mg>
2026-02-21 12:24:36 +0300

    push and ps_simple modifier

--- ft_ps_simple.c
+++ ft_ps_simple.c
@@ -6,7 +6,7 @@
 /*   By: tokyrand <tokyrand@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 20:12:24 by varandri          #+#    #+#             */
-/*   Updated: 2026/02/21 09:34:37 by tokyrand         ###   ########.fr       */
+/*   Updated: 2026/02/21 12:21:52 by tokyrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,15 @@ void	ft_ps_simple(t_list **stack_a, t_list **stack_b, t_ac_list **act_lst)
 		min_index = find_index(*stack_a, min);
 		if (min_index <= size_a / 2 && min_index)
 			rotate_up(min_index, stack_a, act_lst);
-		if (min_index > size_a / 2 && min_index)
+		else if (min_index > size_a / 2 && min_index)
 			rotate_down((size_a - min_index), stack_a, act_lst);
-		if (!min_index)
-			push(stack_a, stack_b, act_lst, "pb");
+		push(stack_a, stack_b, act_lst, "pb");
 		size_a--;
+	}
+	if ((*stack_a)->value > ((*stack_a)->next)->value)
+		ft_swap(stack_a, act_lst, "sa");
+	while (*stack_b)
+	{
+		push(stack_b, stack_a, act_lst, "pa");
 	}
 }
--- ft_push.c
+++ ft_push.c
@@ -3,32 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*   By: tokyrand <tokyrand@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 19:52:37 by varandri          #+#    #+#             */
-/*   Updated: 2026/02/21 00:37:16 by varandri         ###   ########.fr       */
+/*   Updated: 2026/02/21 12:23:58 by tokyrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	remove_front(t_list **list)
-{
-	t_list	*before;
-
-	if (!list || !*list)
-		return ;
-	*list = (*list)->next;
-}
-
 void	push(t_list **from, t_list **to, t_ac_list **act_lst, char *act_name)
 {
-	t_list	*last;
+	t_list	*node;
 
 	if (!from || !*from)
 		return ;
 	if (act_name)
 		new_action(act_lst, act_name);
-	lst_add_front(to, *from);
-	remove_front(from);
+	node = *from;
+	*from = (*from)->next;
+	node->next = NULL;
+	lst_add_front(to, node);
 }
